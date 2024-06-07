cb1631435c04e3b7f7f15c23f72c6106c08ef043
Roman Malolin <rmalolin@gmail.com>
2024-06-08 00:47:02 +0500

    added zustand middleware

--- src/store/useFavoritesStore.ts
+++ src/store/useFavoritesStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 import { Photo } from '../types/Photo';
 
@@ -7,15 +8,20 @@ interface PhotosState {
   toggleFavorite: (photo: Photo) => void;
 }
 
-export const useFavoritesStore = create<PhotosState>(set => ({
-  favorites: [],
-  toggleFavorite: photo =>
-    set(state => {
-      const isFavorite = state.favorites.some(item => item.id === photo.id);
-      const newPhotos = isFavorite
-        ? state.favorites.filter(item => item.id !== photo.id)
-        : [...state.favorites, photo];
+export const useFavoritesStore = create<PhotosState>()(
+  persist(
+    set => ({
+      favorites: [],
+      toggleFavorite: photo =>
+        set(state => {
+          const isFavorite = state.favorites.some(item => item.id === photo.id);
+          const newPhotos = isFavorite
+            ? state.favorites.filter(item => item.id !== photo.id)
+            : [...state.favorites, photo];
 
-      return { favorites: newPhotos };
-    })
-}));
+          return { favorites: newPhotos };
+        })
+    }),
+    { name: 'favorites' }
+  )
+);
